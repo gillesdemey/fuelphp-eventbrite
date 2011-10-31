ebd80ef1855c68455836306a623d5e5485cf88ed
Gilles De Mey <gilles.de.mey@gmail.com>
2011-10-31 16:57:16 +0100

    Edited examples to match new wrapper layout

--- examples/example.php
+++ examples/example.php
@@ -1,34 +1,19 @@
 <?php
-// Use the library
-require_once 'EBAPI.class.php';
 
-// Define user details
-$app_key = '';
-$user_key = '';
+// Copy the configuration file from "/config/eventbrite.php" to "fuel/app/config/eventbrite.php"
+// Fill in your API key and user_key, the config file contains url's where you can request them.
 
-// Initiate the API wrapper
-$api = new EBAPI( $app_key, $user_key );
+// Initiate the wrapper
+$eb = \Eventbrite::forge();
+
 // Load events list
-$result = $api->user_list_events();
+$result = $eb->user_list_events();
 foreach( $result->events as $e )
     echo $e->event->id . "\t" . $e->event->title . "\n";
 
-// Load organizers list
-$result = $api->user_list_organizers();
-foreach( $result->organizers as $o )
-    echo $o->organizer->name . "\n";
-
-// Load organizers list
-$result = $api->user_list_venues();
-foreach( $result->venues as $v )
-    echo $v->venue->name . "\n";
-
-// Load a simple event
-$result = $api->event_get( array( 'id' => 123 ) );
-echo $result->event->url . "\n";
-
-// Test errors
-$result = $api->dumb_method( array( 'id' => 1234 ) );
-echo $api->getError()->error_message . "\n";
+// Load event list by organizer (id is a required value, otherwise the wrapper will give an error)
+$result = $eb->organizer_list_events(array('id' => '1620186468'));
+foreach( $result->events as $e )
+    echo $e->event->id . "\t" . $e->event->title . "\n";
 
 ?>
